1ecbfa0e7e53f752c63721f40765278a496e9905
Kristina Georgieva <kristina.s.georgieva@gmail.com>
2014-05-15 22:04:49 +0200

    Initial Import

---
+++ src/za/co/kristina_georgieva/compactarchaeologist/HomePageActivity.java
@@ -0,0 +1,15 @@
+package za.co.kristina_georgieva.compactarchaeologist;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HomePageActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
